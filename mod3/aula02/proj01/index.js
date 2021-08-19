1a74c60223db5f0416b7fa816b11be577bf7d2c6
Tyfani21 <tyfani.abreu@gmail.com>
2021-08-19 15:54:06 -0400

    js
    Signed-off-by: Tyfani21 <tyfani.abreu@gmail.com>

--- mod3/aula02/proj01/index.js
+++ mod3/aula02/proj01/index.js
@@ -2,3 +2,28 @@ const express = require('express');
 
 const app = express();
 
+const port = 3000;
+
+const filmes = [
+    'Velozes e Furiosos', 
+    'Matrix',
+    'Vingadores'
+];
+
+app.get('/',(req, res) =>{
+    res.send("Bem vindos ao site, lista de filmes");
+});
+
+app.get('/filmes', (req, res) =>{
+    res.send(filmes);
+});
+
+app.get('/filmes/:id', req, res =>{
+    const id = req.params.id -1;
+    const filme = filmes[id];
+    res.send(filme)
+})
+
+app.listen(port, () =>{
+    console.info(`App está rodando em: http://localhost:${port}/`)
+});
