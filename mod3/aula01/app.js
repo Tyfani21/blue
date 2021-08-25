55230b4a5e9fd59a846b3b7bda0cf236a1842c47
Tyfani21 <tyfani.abreu@gmail.com>
2021-08-25 16:43:30 -0400

    games
    Signed-off-by: Tyfani21 <tyfani.abreu@gmail.com>

--- mod3/aula01/app.js
+++ mod3/aula01/app.js
@@ -1,1 +1,7 @@
-console.log('Olá Mundo!')
+const calculadora = require('./calculadora');
+
+console.log(calculadora.nome);
+console.log('soma: ' + calculadora.soma(1, 2));
+console.log('Sub: ' + calculadora.sub(5,3));
+console.log('multi: ' + calculadora.multi(3,5));
+console.log('div: ' + calculadora.div(10, 2));
--- mod3/aula01/calculadora.js
+++ mod3/aula01/calculadora.js
@@ -1,5 +1,27 @@
 const nome = 'Calculadora';
 
-function soma(a, b){
-    return a
-}
+function soma(num1, num2) {
+    return num1 + num2;
+  };
+  
+  function sub(a, b) {
+    return a - b;
+  };
+  
+  function multi(a, b) {
+    return a * b;
+  };
+  
+  function div(a, b) {
+    return a / b;
+  };
+  
+  //módulo - contem metodos, funcioes, regras, valdacoes - regra de negocio.
+  
+  module.exports = {
+    soma,
+    sub,
+    multi,
+    div,
+    nome
+  }
